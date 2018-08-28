1787b6b468e9c543c5158c67a27fd52e53e0a049
leop3 <leoarancibiak@hotmail.com>
2018-08-28 17:35:16 -0300

    updating

--- routes.js
+++ routes.js
@@ -4,38 +4,32 @@ var routes = ($stateProvider, $urlRouterProvider) => {
         .state('home', {
             url: "/",
             templateUrl: "partials/home.html",
-            // controller: "homeController"
         })
         .state('coaching', {
             url: "/",
             templateUrl: "partials/coaching.html",
-            // controller: "finalizarPedidoController as finPedido"
 
         })
-        // .state('ecommerce', {
-        //     url: "/",
-        //     templateUrl: "partials/ecommerce.html",
-        //     // controller: "finalizarPedidoController as finPedido"
+        .state('ecommerce', {
+            url: "/",
+            templateUrl: "partials/ecommerce.html",
 
-        // })
-        // .state('exportacion', {
-        //     url: "/",
-        //     templateUrl: "partials/exportacion.html",
-        //     // controller: "finalizarPedidoController as finPedido"
+        })
+        .state('exportacion', {
+            url: "/",
+            templateUrl: "partials/exportacion.html",
 
-        // })
-        // .state('algo', {
-        //     url: "/",
-        //     templateUrl: "partials/nosequees.html",
-        //     // controller: "finalizarPedidoController as finPedido"
+        })
+        .state('logistica', {
+            url: "/",
+            templateUrl: "partials/logistica.html",
 
-        // })
-        // .state('transporte', {
-        //     url: "/",
-        //     templateUrl: "partials/transporte.html",
-        //     // controller: "finalizarPedidoController as finPedido"
+        })
+        .state('trading', {
+            url: "/",
+            templateUrl: "partials/trading.html",
 
-        // })
+        })
 
     $urlRouterProvider.otherwise("/")
 
